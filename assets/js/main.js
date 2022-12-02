c2e1667a02c07bcd3d7c2b2d7793edc182e57f05
zabih <107562768+minhaj-313@users.noreply.github.com>
2022-12-02 12:50:00 +0530

    linktree

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-input",{
+    strings:["Full Stack Developer", "Frontend Developer", "Backend Developer", "Open Source Contributer", "Freelancer","Web Developer", "Web Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+})
